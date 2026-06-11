7ba708ceea9575741558cf24f788015ee1e7aee4
codebyvidhi <vidhihooda008@gmail.com>
2026-06-11 10:49:26 +0530

    Time: 423 ms (70.68%), Space: 50.7 MB (75.03%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.py
@@ -0,0 +1,49 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def sortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        # Base case
+        if not head or not head.next:
+            return head
+        
+        # Step 1: Split list into two halves
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        mid = slow.next
+        slow.next = None
+        
+        # Step 2: Sort each half
+        left = self.sortList(head)
+        right = self.sortList(mid)
+        
+        # Step 3: Merge sorted halves
+        return self.merge(left, right)
+    
+    def merge(self, l1, l2):
+        dummy = ListNode(0)
+        tail = dummy
+        
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        
+        tail.next = l1 if l1 else l2
+        return dummy.next
+
+        
